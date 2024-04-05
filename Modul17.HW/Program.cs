3f2796b7aa0620936b0ef9c698ba3ed2262310cb
Илья Зуев <zueviliya777@gmail.com>
2024-04-05 03:37:34 +0300

    Добавьте файлы проекта.

---
+++ Modul17.HW/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Modul17.HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account regularAccount = new Account { Type = "Обычный", Balance = 70000 };
+            Account salaryAccount = new Account { Type = "Зарплатный", Balance = 10000 };
+
+            Calculator.CalculateInterest(regularAccount);
+            Calculator.CalculateInterest(salaryAccount);
+
+            Console.WriteLine($"Доля обычного аккаунта: {regularAccount.Interest}");
+            Console.WriteLine($"Доля зарплатного аккаунта: {salaryAccount.Interest}");
+
+        }
+    }
+}
